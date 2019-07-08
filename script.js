401737cc6fa1562d48eb4afdbfd603014d18e18c
j122885 <44957340+j122885@users.noreply.github.com>
2019-07-08 17:39:59 -0500

    Added js file to add functionality to calc- NOT FINISHED
    I somehow need to get the numbers to add. I'm trying to figure out how to add value to itself so it will add the numbers inside the textbox. Need to finish the clear and operative numbers.

---
+++ script.js
@@ -0,0 +1,56 @@
+function ButtonON() {
+    document.getElementById("TextField").value = "9"; /* Fix this button so it turns on the calculator */
+}
+function ButtonC() {
+    document.getElementById("TextField").value = ""; /* Fix this button clears the calculator */
+}
+function Buttonclear() {
+    
+    document.getElementById("TextField").value = ""; /* Fix this button clears the calculator */
+    
+}
+
+function Button9() {
+    
+    document.getElementById("TextField").value = "9" ;
+}
+
+function Button8() {
+    document.getElementById("TextField").value = "8";
+}
+
+function Button7() {
+    document.getElementById("TextField").value = "7";
+}
+
+function Button6() {
+    document.getElementById("TextField").value = "6";
+}
+
+function Button5() {
+    document.getElementById("TextField").value = "5";
+}
+
+
+function Button4() {
+    document.getElementById("TextField").value = "4";
+}
+
+
+function Button3() {
+    document.getElementById("TextField").value = "3";
+}
+function Button2() {
+    document.getElementById("TextField").value = "2";
+}
+
+function Button1() {
+    document.getElementById("TextField").value = "1";
+}
+function Buttonplus() {
+    document.getElementById("TextField").value = "9"; /* Fix this button does addition */
+
+}
+function Button9minus() {
+    document.getElementById("TextField").value = "9"; /* Fix this button does subtraction */
+}
